e9aefb908fa9685ce62458b78cbd4d1d7402ec02
Mitxel Moriana <mmoriana@cvaconsulting.com>
2016-09-23 13:39:59 +0200

    update intensiva script

--- scripts/intensiva.js
+++ scripts/intensiva.js
@@ -3,10 +3,10 @@
 
         robot.respond(/¿?(me toca|tengo) intensiva( esta semana)?\?/i, function (res) {
             var sender = res.message.user.name.toLowerCase();
-            return res.reply("Hola " + sender + ", aún sé si tienes intensiva (sadpanda). Pregúntale a @durena");
+            return res.reply("Hola " + sender + ", aún no sé si tienes intensiva (sadpanda). Pregúntale a @durena");
         });
 
-        robot.respond(/¿?qui[eé]n tiene intensiva( esta semana)?\?/i, function (res) {
+        robot.respond(/¿?(quien tiene|a quién le toca|quién tiene|a quien le toca)? intensiva( esta semana)?\?/i, function (res) {
             return res.reply("No sé aún a quién le toca intensiva esta semana. Me siento un poco inútil (sadpanda). Pero le puedes preguntar a @durena");
         });
 
